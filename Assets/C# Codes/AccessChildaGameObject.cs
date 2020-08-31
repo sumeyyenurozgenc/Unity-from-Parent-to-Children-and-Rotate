4eaa87e36aad12309d7c9270e5fae7ba202afd1b
sumeyyenurozgenc <59045890+sumeyyenurozgenc@users.noreply.github.com>
2020-08-31 23:18:13 +0300

    Add files via upload

---
+++ Assets/C# Codes/AccessChildaGameObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccessChildaGameObject : MonoBehaviour
+{
+    public GameObject parentCube;
+    private float rotationNumber = 50f;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up, rotationNumber * Time.deltaTime);
+        transform.GetChild(0).Rotate(Vector3.left, rotationNumber * Time.deltaTime);
+        transform.GetChild(1).Rotate(Vector3.right, rotationNumber * Time.deltaTime);
+        transform.GetChild(2).Rotate(Vector3.up, rotationNumber * Time.deltaTime);
+        transform.GetChild(3).Rotate(Vector3.left, rotationNumber * Time.deltaTime);
+        transform.GetChild(4).Rotate(Vector3.right, rotationNumber * Time.deltaTime);
+    }
+}
